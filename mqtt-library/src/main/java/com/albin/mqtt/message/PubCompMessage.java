0e21ed78645774b1a6d238b96617b4dd85cbbf65
Albin Theander <Albin.Theander@jayway.com>
2011-09-11 22:08:24 +0200

    Added support for PUBCOMP. Still untested

---
+++ mqtt-library/src/main/java/com/albin/mqtt/message/PubCompMessage.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright 2011 Albin Theander
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.albin.mqtt.message;
+
+import java.io.IOException;
+
+public class PubCompMessage extends RetryableMessage {
+
+	public PubCompMessage(int messageId) {
+		super(Type.PUBCOMP);
+		setMessageId(messageId);
+	}
+
+	public PubCompMessage(Header header) throws IOException {
+		super(header);
+	}
+}
