9386c7b35ef418ca75ca7f80c1e84c84ff399894
plucury <plucury@gmail.com>
2012-09-11 17:26:17 +0800

    Added the read method of UnsubscribeMessage with unittest.

--- mqtt-library/src/main/java/com/albin/mqtt/message/MessageInputStream.java
+++ mqtt-library/src/main/java/com/albin/mqtt/message/MessageInputStream.java
@@ -67,6 +67,15 @@ public class MessageInputStream implements Closeable {
 		case SUBSCRIBE:
 			msg=new SubscribeMessage(header);
 			break;
+		case UNSUBSCRIBE:
+			msg=new UnsubscribeMessage(header);
+			break;
+		case PINGREQ:
+			msg=new PingReqMessage(header);
+			break;
+		case DISCONNECT:
+			msg=new DisconnectMessage(header);
+			break;
 		default:
 			throw new UnsupportedOperationException(
 					"No support for deserializing " + header.getType()
--- mqtt-library/src/main/java/com/albin/mqtt/message/UnsubscribeMessage.java
+++ mqtt-library/src/main/java/com/albin/mqtt/message/UnsubscribeMessage.java
@@ -15,8 +15,10 @@
  ******************************************************************************/
 package com.albin.mqtt.message;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +26,7 @@ import java.util.List;
 import com.albin.mqtt.util.FormatUtil;
 
 public class UnsubscribeMessage extends RetryableMessage {
-	
+
 	private List<String> topics = new ArrayList<String>();
 
 	public UnsubscribeMessage(String topic) {
@@ -33,26 +35,45 @@ public class UnsubscribeMessage extends RetryableMessage {
 		topics.add(topic);
 	}
 	
+	public void addTopic(String topic){
+		topics.add(topic);
+	}
+
+	public UnsubscribeMessage(Header header) throws IOException {
+		super(header);
+	}
+
 	@Override
 	protected int messageLength() {
 		int length = 2; // message id length
-		for(String topic: topics) {
+		for (String topic : topics) {
 			length += FormatUtil.toMQttString(topic).length;
 		}
 		return length;
 	}
-	
+
 	@Override
 	protected void writeMessage(OutputStream out) throws IOException {
 		DataOutputStream dos = new DataOutputStream(out);
 		super.writeMessage(dos);
-		for(String topic: topics) {
+		for (String topic : topics) {
 			dos.writeUTF(topic);
 		}
 		dos.flush();
 	}
-	
 
+	
+	@Override
+	protected void readMessage(InputStream in, int msgLength) throws IOException {
+		super.readMessage(in, msgLength);
+		DataInputStream dis=new DataInputStream(in);
+		int pos=2;
+		while(pos < msgLength){
+			topics.add(dis.readUTF());
+			pos+=FormatUtil.toMQttString(topics.get(topics.size()-1)).length;
+		}
+	}
+	
 	@Override
 	public void setQos(QoS qos) {
 		if (qos != QoS.AT_LEAST_ONCE) {
@@ -62,18 +83,24 @@ public class UnsubscribeMessage extends RetryableMessage {
 		}
 		super.setQos(qos);
 	}
-	
+
 	@Override
 	public void setDup(boolean dup) {
 		if (dup == true) {
-			throw new IllegalArgumentException("SUBSCRIBE can't set the DUP flag.");
+			throw new IllegalArgumentException(
+					"SUBSCRIBE can't set the DUP flag.");
 		}
 		super.setDup(dup);
 	}
-	
+
 	@Override
 	public void setRetained(boolean retain) {
-		throw new UnsupportedOperationException("SUBSCRIBE messages don't use the RETAIN flag");
+		throw new UnsupportedOperationException(
+				"SUBSCRIBE messages don't use the RETAIN flag");
+	}
+
+	public List<String> getTopics() {
+		return topics;
 	}
 
 }
